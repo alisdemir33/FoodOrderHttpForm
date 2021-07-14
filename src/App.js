082ab421c8b842fee1ddca8b4fb5ac872ba593d1
alisdemir33 <alisdemir33@gmail.com>
2021-07-14 23:56:16 +0300

    http added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,37 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
+import OrderForm from './components/Order/OrderForm'
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
+  const [orderFormIsShown, setOrderFormIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
+    setOrderFormIsShown(false);
   };
 
   const hideCartHandler = () => {
+    setCartIsShown(false);  
+  };
+
+  const showOrderFormHandler = () => {
+    ;debugger
+    setOrderFormIsShown(true);
     setCartIsShown(false);
+  };
+
+  const hideOrderFormHandler = () => {
+    setOrderFormIsShown(false);
   };
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      {cartIsShown && <Cart onClose={hideCartHandler} onShowOrderForm={showOrderFormHandler} />}
+      {orderFormIsShown && <OrderForm onClose={hideOrderFormHandler} />}
       <Header onShowCart={showCartHandler} />
+    
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -45,7 +45,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        {hasItems && <button className={classes.button}>Order</button>}
+        {hasItems && <button className={classes.button} onClick={props.onShowOrderForm}>Order</button>}
       </div>
     </Modal>
   );
